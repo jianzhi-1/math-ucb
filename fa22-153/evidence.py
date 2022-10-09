fcffb4c3876988beab55e95744efbb6259229555
Jianzhi Wang <jianzhiw00@gmail.com>
2022-10-08 19:50:59 -0700

    Update evidence.py

--- fa22-153/evidence.py
+++ fa22-153/evidence.py
@@ -1,5 +1,9 @@
 def evidence(X, y):
-    n = len(X)
+    n, p = X.shape
     betahat = np.linalg.inv(X.T@X)@X.T@y
-    p = len(betahat)
     return gamma(p/2)*gamma((n-p-1)/2)/((np.linalg.norm(X@betahat))**p)/((np.linalg.norm(y - X@betahat))**(n-p))
+
+def log_evidence(X, y):
+    n, p = X.shape
+    betahat = np.linalg.inv(X.T@X)@X.T@y
+    return np.log(gamma(p/2)) + np.log(gamma((n-p-1)/2)) - p*np.log(np.linalg.norm(X@betahat)) - (n - p)*np.log(np.linalg.norm(y - X@betahat))
