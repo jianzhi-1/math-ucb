2eaa6e8a3004e8dd66ec7a7893f66345da2415f7
Jianzhi Wang <jianzhiw00@gmail.com>
2022-10-08 20:22:58 -0700

    Update aic.py

--- fa22-153/aic.py
+++ fa22-153/aic.py
@@ -1,5 +1,4 @@
 def aic(X, y):
-    n = len(X)
+    n, p = X.shape
     betahat = np.linalg.inv(X.T@X)@X.T@y
-    p = len(betahat)
     return n*np.log(2.*np.pi*np.sum((y - X@betahat)**2)/n) + n + 2.*p
