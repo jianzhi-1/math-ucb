f2cc721e0b2cdb409f84b4e940b0ca0c0d071f95
Jianzhi Wang <jianzhiw00@gmail.com>
2022-10-08 19:55:36 -0700

    Create polynomial_features.py

---
+++ fa22-153/polynomial_features.py
@@ -0,0 +1,7 @@
+def polynomial_features(X, k):
+    # generates polynomial features
+    ls = []
+    ls.append(np.ones(len(X)))
+    for i in range(1, k + 1):
+        ls.append(X**k)
+    return np.vstack(ls).T
