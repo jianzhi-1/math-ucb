fbea2bb12a6148324914b0aab7619d6e8e21d917
Jianzhi Wang <jianzhiw00@gmail.com>
2022-11-06 09:00:44 -0800

    Create acf.py

---
+++ fa22-153/acf.py
@@ -0,0 +1,25 @@
+def acf(y):
+    """Returns ACF and significance band of time series."""
+    n = len(y)
+    ybar = np.sum(y)/n
+    sos = np.sum((y - ybar)**2)
+    acfls = np.zeros(n)
+    for i in range(n):
+        yfront = y[:n-i].copy()
+        yback = y[i:].copy()
+        yfront -= ybar
+        yback -= ybar
+        acfls[i] = np.sum(yfront*yback)/sos
+    sig_band = 1.96*(n**(-0.5))
+    return acfls, sig_band
+
+### Plotting
+acf_ls, sig_band = acf(y)
+plt.figure(figsize=(15,10))
+plt.stem(acf_ls[:100])
+plt.axhline(sig_band, color="red", linestyle='--')
+plt.axhline(-sig_band, color='red', linestyle='--')
+plt.xlabel('lag') 
+plt.ylabel('ACF')
+plt.title('ACF Plot')
+plt.show()
