dfad845651fad8d0973cd88d89739d0fdc77e275
Jianzhi Wang <jianzhiw00@gmail.com>
2023-08-22 06:41:41 -0700

    Update linear_regression.py

--- fa22-153/linear_regression.py
+++ fa22-153/linear_regression.py
@@ -3,11 +3,12 @@ def linear_regression(X, y):
     n = len(X) # number of data points
     betahat = np.linalg.inv(X.T@X)@X.T@y
     p = len(betahat) # number of parameters
-    sigmahat = np.sqrt((1./(n - 2))*np.linalg.norm(y - X@betahat)**2)
+    sigmahat = np.sqrt((1./(n - p))*np.linalg.norm(y - X@betahat)**2)
     beta_confidence_interval = []
     sigma_confidence_interval = None
+    
     for i in range(len(betahat)):
-        beta_confidence_interval.append(stats.t.interval(0.95, n - 2, loc=betahat[i], scale=sigmahat*np.sqrt(np.linalg.inv(X.T@X)[i][i])))
+        beta_confidence_interval.append(stats.t.interval(0.95, n - p, loc=betahat[i], scale=sigmahat*np.sqrt(np.linalg.inv(X.T@X)[i][i])))
     
     sbeta = np.linalg.norm(y - X@betahat, 2)**2 # sum of squared error
     sigma_higher_confidence = np.sqrt(sbeta/stats.chi2.interval(0.95, n - p)[0])
