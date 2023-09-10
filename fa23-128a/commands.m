d26241c564f59cf59cb262f42372f0ebfaab94ee
Jianzhi Wang <jianzhiw00@gmail.com>
2023-09-10 14:22:39 -0700

    Update commands.m

--- fa23-128a/commands.m
+++ fa23-128a/commands.m
@@ -1,3 +1,6 @@
-params = struct("tol", 1e-2, "MaxIt", 20)
+params = struct("tol", 1e-2, "MaxIt", 20);
 f = @(x) 2*x*cos(2*x) - (x - 2)^2;
-[fun, dfun, x, out] = NewtonMethod(f, df, 3.5, params)
+[fun, dfun, x, out] = NewtonMethod(f, df, 3.5, params);
+[x, out] = SecantMethod(f, 2.45, 2.55, params);
+
+format longg; % more decimal places
