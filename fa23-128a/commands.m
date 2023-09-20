ad9a63ee68f9f19a3e0781289d9259becc57e7d2
Jianzhi Wang <jianzhiw00@gmail.com>
2023-09-19 20:44:29 -0700

    Update commands.m

--- fa23-128a/commands.m
+++ fa23-128a/commands.m
@@ -9,6 +9,7 @@ g = @(x) 1 + sin(x)^2;
 x0 = 1;
 params = struct("tol", 2e-14, "MaxIt", 30);
 [x, out] = SteffensenMethod(g, x0, params)
+% out.x stores the Steffensen computes i.e. p0(n)
 
 for i = 1:17;
   x(i) - (x(i + 1) - x(i))^2/(x(i+2) - 2*x(i+1) + x(i));
