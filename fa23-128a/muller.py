30986dcf31693e7263317d0312b012d667319de1
Jianzhi Wang <jianzhiw00@gmail.com>
2023-09-19 21:58:05 -0700

    Update muller.py

--- fa23-128a/muller.py
+++ fa23-128a/muller.py
@@ -33,3 +33,7 @@ def muller(f, p0, p1, p2, tol=1e-6, maxit=40):
         delta2 = (f(p2) - f(p1))/h2
         d = (delta2 - delta1)/(h1 + h2)
         i = i + 1
+
+### Use cases
+# print(muller(lambda x: x**4 - 2*x**3 - 12*x**2 + 16*x - 40, 0.0, -0.1, -0.2))
+# print(muller(lambda x: 16*x**4 + 88*x**3 + 159*x**2 + 76*x - 240, -1.6, -1.55, -1.5))
