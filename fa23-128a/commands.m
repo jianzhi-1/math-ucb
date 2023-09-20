17f90546416ead585b61b949ed2d3905207729b1
Jianzhi Wang <jianzhiw00@gmail.com>
2023-09-19 20:43:19 -0700

    Update commands.m

--- fa23-128a/commands.m
+++ fa23-128a/commands.m
@@ -4,3 +4,12 @@ f = @(x) 2*x*cos(2*x) - (x - 2)^2;
 [x, out] = SecantMethod(f, 2.45, 2.55, params);
 
 format longg; % more decimal places
+
+g = @(x) 1 + sin(x)^2;
+x0 = 1;
+params = struct("tol", 2e-14, "MaxIt", 30);
+[x, out] = SteffensenMethod(g, x0, params)
+
+for i = 1:17;
+  x(i) - (x(i + 1) - x(i))^2/(x(i+2) - 2*x(i+1) + x(i));
+end
